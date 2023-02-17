0a650ab7c9889030e1994410894e9a271b529393
Tim <gurov-95@yandex.ru>
2023-02-17 18:08:29 +0800

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,27 +1,53 @@
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
-  slidesPerView: 3,
-  spaceBetween: 48,
-  centeredSlides: true,
+
   speed: 600,
 
   pagination: {
     el: '.service__pagination',
     clickable: true
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 28,
+    },
+    750: {
+      slidesPerView: 2,
+      spaceBetween: 36,
+    },
+    1100: {
+      slidesPerView: 3,
+      spaceBetween: 48,
+    },
   }
 });
 
 const swiperTest = new Swiper('.testimonials__slider', {
   direction: 'horizontal',
   loop: true,
-  slidesPerView: 3,
-  spaceBetween: 32,
+  height: 391,
 
   navigation: {
     nextEl: '.testimonials__next',
     prevEl: '.testimonials__prev',
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 32,
+    },
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  }
 });
 
 
@@ -35,4 +61,24 @@ burgerMenu.addEventListener('click', function () {
   navMenu.classList.toggle('open');
   overlay.classList.toggle('show');
 });
+
+setInterval(() => {
+  if  ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+    $('.burger').addClass('burger--follow')
+  }
+  else {
+    $('.burger').removeClass('burger--follow')
+  }
+}, 0);
+$('.burger, .overlay, .header__top a').on('click' , function(e) {
+  e.preventDefault()
+  $('.header__top').toggleClass('header__top--open')
+  $('.overlay').toggleClass('overlay--show')
+  // $('.burger').toggleClass('burger--open')
+
+})
+
+$('.footer__column-title--slide').on('click', function() {
+  $(this).next().slideToggle();
+})
 
